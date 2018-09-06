647748e9c5f4cd00f4b71c3a0071bf5207b93869
Dan Hintz <dhintz@live.com>
2018-09-06 23:17:11 +0000

    automatically backed up by learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,11 +1,10 @@
 class Cat
   attr_accessor :mood
-  attr_reader :name, :species
+  attr_reader :name
   
   def initialize(name)
     @name = name
     @mood = "nervous"
-    @species = "Cat"
   end
   
   
